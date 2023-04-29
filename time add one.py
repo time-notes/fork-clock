36aa300ef05cee4413d03ff358987e90182af178
time-notes <58352675+time-notes@users.noreply.github.com>
2023-04-29 10:30:50 +0800

    Revert "Create time add one.py"

--- time add one.py
+++
@@ -1,8 +0,0 @@
-{
-    "error": {
-        "message": "You exceeded your current quota, please check your plan and billing details.",
-        "type": "insufficient_quota",
-        "param": null,
-        "code": null
-    }
-}
