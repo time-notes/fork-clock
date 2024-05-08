85f8fd20cb8b8fcab9cc6912788236896d76e68b
time-notes <58352675+time-notes@users.noreply.github.com>
2024-05-08 08:42:44 +0800

    Update fork-clock.py

--- fork-clock.py
+++ fork-clock.py
@@ -3,9 +3,9 @@ import time
 def pomodoro():
     print("Let's start focusing!")
     for i in range(4): #运行四次专注时间和短休息时间
-        time.sleep(1500) #专注时间为25分钟，即1500秒
+        time.sleep(2000) #专注时间为25分钟，即1500秒
         print("Time is up! Take a short break.")
-        time.sleep(300) #短休息时间为5分钟，即300秒
+        time.sleep(200) #短休息时间为5分钟，即300秒
     time.sleep(900) #长休息时间为15分钟，即900秒
     print("Congratulations! You have completed a pomodoro cycle. Take a long break.")
 
